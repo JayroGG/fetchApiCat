5bdfc66fe6c5479d00164445dec49e25f3c162dc
José Jayro Gómez Garcia <jayro@Mac.local>
2023-01-12 14:57:17 -0600

    The same but with asyn await, can't wait for top level await in ES22

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-console.log("Hello World")
-
 const URL = "http://api.thecatapi.com/v1/images/search"
 
 fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url          
-    })
+    // .then(res => res.json())
+    // .then(data => {
+    //     console.log(data)
+    //     const img = document.querySelector('img')
+    //     img.src = data[0].url          
+    // })
+async function getKitty(url){
+    const res = await fetch(url)
+    const data = await res.json()
+    const img = document.querySelector('img')
+    img.src = data[0].url 
+}
+
+getKitty(URL)
 
 let refresh = document.getElementById('refresh')
 refresh.addEventListener('mouseover', () => location.reload())
